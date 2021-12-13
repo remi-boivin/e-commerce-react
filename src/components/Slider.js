48d9d55e59b666d7759700102a01df9cd557ec8e
Rémi Boivin <deerhair_sannyasin@aleeas.com>
2021-12-13 16:36:03 +0100

    reorganiser react app architecture

---
+++ src/components/Slider.js
@@ -0,0 +1,39 @@
+// import '../css/Nav.css';
+
+function Slider() {
+  return (
+    <section class="ftco-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h2 class="heading-section mb-5">Carousel #08</h2>
+                </div>
+                <div class="col-md-12">
+                    <div class="featured-carousel owl-carousel">
+                        <div class="item">
+                            <div class="row justify-content-center">
+                                <div class="col-md-11">
+                                    <div class="testimony-wrap d-md-flex">
+                                        <div class="img" style={{}}></div>
+                                            <div class="text text-center p-4 py-xl-5 px-xl-5 d-flex align-items-center">
+                                                <div class="desc w-100">
+                                                    <p class="h3 mb-5">"Incredible services and amazing customer support</p>
+                                                    <div class="pt-4">
+                                                    <p class="name mb-0">&mdash; Joy Smith</p>
+                                                    <span class="position">Project Manager</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  );
+}
+
+export default Slider;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './css/index.css';
+import Nav from './components/Nav';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Slider from './components/Slider';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <div><Nav /> */}
+    <Slider />
   </React.StrictMode>,
   document.getElementById('root')
 );
